e4894925a5f1484744b181e423fe9a1529683437
tada5hi <peter.placzek1996@gmail.com>
2024-01-08 10:00:31 +0100

    feat: don't require and set default values for action input

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,29 @@
-import { getInput, info } from '@actions/core';
+import { getInput, info, setFailed } from '@actions/core';
 import type { Package } from 'workspaces-publish';
 import { publish } from 'workspaces-publish';
 
 export async function run(): Promise<void> {
-    const token = getInput('token', { required: true });
-    const registry = getInput('registry', { required: true });
-    const cwd = getInput('cwd') || process.cwd();
+    const token = getInput('token') || undefined;
+    const registry = getInput('registry') || undefined;
+    const cwd = getInput('cwd') || undefined;
 
-    const packages : Package[] = await publish({
-        token,
-        registry,
-        cwd,
-        rootPackage: true,
-    });
+    try {
+        const packages: Package[] = await publish({
+            token,
+            registry,
+            cwd,
+            rootPackage: true,
+        });
 
-    if (packages.length === 0) {
-        info('No packages need to be published.');
-        return;
-    }
+        if (packages.length === 0) {
+            info('No packages need to be published.');
+            return;
+        }
 
-    for (let i = 0; i < packages.length; i++) {
-        info(`published ${packages[i].content.name}@${packages[i].content.version}`);
+        for (let i = 0; i < packages.length; i++) {
+            info(`published ${packages[i].content.name}@${packages[i].content.version}`);
+        }
+    } catch (e) {
+        if (e instanceof Error) setFailed(e.message);
     }
 }
